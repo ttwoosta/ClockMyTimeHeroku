609e27bc658ae9c7035e5f43cbcd4899c69aabfa
Tu Tong <ttwoosta@gmail.com>
2020-04-25 21:10:10 -0400

    Update heroku.py

--- gettingstarted/settings/heroku.py
+++ gettingstarted/settings/heroku.py
@@ -12,7 +12,7 @@ from gettingstarted.settings.base import *
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "3223123sasadwTES"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
